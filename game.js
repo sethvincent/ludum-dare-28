2caf8e85b8a753f86f10adc3b3d201c4c13ef628
sethvincent <sethvincent@gmail.com>
2013-12-15 03:44:54 -0800

    starting with weird canvas animations

--- game.js
+++ game.js
@@ -13,7 +13,14 @@ var Keyboard = require('crtrdg-keyboard');
 
 var Log = require('./ui/log');
 var Terminal = require('./ui/terminal');
+var randomInt = require('./util/math').randomInt;
 var randomRGB = require('./util/math').randomRGB;
+var randomRGBA = require('./util/math').randomRGBA;
+
+
+/*
+* create game object
+*/
 
 var game = new Game({
   canvas: 'game',
@@ -21,6 +28,28 @@ var game = new Game({
   height: window.innerHeight - 80,
   backgroundColor: '#323230'
 });
+
+var size = 5;
+var columns = game.width / size;
+var rows = game.height / size;
+
+game.on('draw', function(c){
+
+  for (var w=0; w<columns; w+=randomInt(5, 20)){
+    c.fillStyle = randomRGBA(0, 255, 0, 200, 0, 211, 0.5);
+    //c.fillRect(size*w, 0, randomInt(1, 3), game.height);    
+  }
+
+  for (var h=0; h<rows; h+=randomInt(5, 20)){
+    c.fillStyle = randomRGBA(h*10, h*20, 0, 0, 0, 1, 0.6);
+    c.fillRect(0, size*h, game.width, randomInt(1, 2));
+  }
+
+})
+
+/*
+* create player characters
+*/
 
 var players = {
   eika: {
@@ -34,10 +63,12 @@ var players = {
 };
 
 var currentPlayer = players.eika;
+playerElUpdate(currentPlayer);
 
 function switchPlayer(){
   if (currentPlayer.name === 'eika') currentPlayer = players.fullerton;
   else currentPlayer = players.eika;
+  playerElUpdate(currentPlayer);
 }
 
 function playerElUpdate(player){
@@ -45,7 +76,6 @@ function playerElUpdate(player){
   el.innerHTML = player.name;
 }
 
-playerElUpdate(currentPlayer);
 
 /*
 * terminal
@@ -56,7 +86,6 @@ var terminal = new Terminal();
 terminal.on('command', function(message, command, option){
   currentPlayer.log.add(message);
   switchPlayer();
-  playerElUpdate(currentPlayer);
   var color = randomRGB(0, 125, 0, 25, 0, 55);
   game.backgroundColor = color;
 });
